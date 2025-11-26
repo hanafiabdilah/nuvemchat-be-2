f88848563327fc2c5d3ea8fc2981cef178c7bc7c
Hanafi Abdilah <handilah81@gmail.com>
2025-11-26 10:39:07 +0700

    ok

--- routes/webhook.php
+++ routes/webhook.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/webhook/chat/{id}', function($id){
     Log::info("Webhook subscribed for connection ID: {$id}", request()->all());
+
+    return request()->hub_challenge;
 })->name('webhook.chat');
 
 Route::post('/webhook/chat/{id}', function($id){
