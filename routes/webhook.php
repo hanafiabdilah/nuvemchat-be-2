a49ee1b5991b90bc5bbb6bf52c664b1ff50988b3
Hanafi Abdilah <handilah81@gmail.com>
2025-11-26 10:31:54 +0700

    ok

--- routes/webhook.php
+++ routes/webhook.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/webhook/chat/{id}', function($id){
+    Log::info("Webhook subscribed for connection ID: {$id}", request()->all());
+})->name('webhook.chat');
+
 Route::post('/webhook/chat/{id}', function($id){
     Log::info("Webhook chat received for connection ID: {$id}", request()->all());
 })->name('webhook.chat');
