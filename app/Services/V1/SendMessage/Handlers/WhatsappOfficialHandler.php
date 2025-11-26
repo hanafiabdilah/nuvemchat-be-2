bc7b501cb3b0075aa4cba7153d8a98fd005cac41
Hanafi Abdilah <handilah81@gmail.com>
2025-11-26 15:19:31 +0700

    ssend message whatsapp

--- app/Services/V1/SendMessage/Handlers/WhatsappOfficialHandler.php
+++ app/Services/V1/SendMessage/Handlers/WhatsappOfficialHandler.php
@@ -2,11 +2,33 @@
 
 namespace App\Services\V1\SendMessage\Handlers;
 
+use App\Enums\Message\MessageType;
+use App\Enums\Message\SenderType;
 use App\Models\Connection;
+use App\Models\Conversation;
 use App\Services\V1\SendMessage\SendMessageHandlerInterface;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Support\Facades\Http;
 
 class WhatsappOfficialHandler implements SendMessageHandlerInterface
 {
+    public function getConversationId(array $payload): string
+    {
+        return $payload['contacts'][0]['wa_id'];
+    }
+
+    public function getMessageId(array $payload): string
+    {
+        return $payload['messages'][0]['id'];
+    }
+
+    public function getMessageSentAt(array $payload): Carbon
+    {
+        return Carbon::now(); // Assuming message is sent now
+    }
+
+
     public function handle(Connection $connection, array $data)
     {
         validator($data, [
@@ -14,5 +36,35 @@ class WhatsappOfficialHandler implements SendMessageHandlerInterface
             'message' => 'required|string',
         ])->validate();
 
+        try {
+            $response = Http::withToken($connection->credentials['access_token'])
+                ->post('https://graph.facebook.com/v22.0/' . $connection->credentials['phone_number_id'] . '/messages', [
+                    'messaging_product' => 'whatsapp',
+                    'recipient_type' => 'individual',
+                    'to' => $data['to'],
+                    'type' => 'text',
+                    'text' => [
+                        'body' => $data['message'],
+                    ],
+                ]);
+
+            $responseArray = $response->json();
+
+            $conversation = Conversation::firstOrCreate([
+                'connection_id' => $connection->id,
+                'external_id'   => $this->getConversationId($responseArray),
+            ]);
+
+            $conversation->messages()->create([
+                'external_id' => $this->getMessageId($responseArray),
+                'sender_type' => SenderType::Outgoing,
+                'message_type' => MessageType::Text,
+                'body' => $data['message'],
+                'sent_at' => $this->getMessageSentAt($responseArray),
+                'meta' => $responseArray,
+            ]);
+        } catch (\Throwable $th) {
+            throw new Exception('Failed to send WhatsApp message');
+        }
     }
 }
--- app/Services/V1/SendMessage/SendMessageHandlerInterface.php
+++ app/Services/V1/SendMessage/SendMessageHandlerInterface.php
@@ -3,8 +3,13 @@
 namespace App\Services\V1\SendMessage;
 
 use App\Models\Connection;
+use Carbon\Carbon;
 
 interface SendMessageHandlerInterface
 {
+    public function getConversationId(array $payload): string;
+    public function getMessageId(array $payload): string;
+    public function getMessageSentAt(array $payload): Carbon;
+
     public function handle(Connection $connection, array $data);
 }
