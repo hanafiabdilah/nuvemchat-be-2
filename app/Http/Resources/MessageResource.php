5afccf74a99e01af24d5640d8c2c61e5fcc13a0f
Hanafi Abdilah <handilah81@gmail.com>
2025-11-27 22:47:30 +0700

    attachment url in message resource

--- app/Http/Resources/MessageResource.php
+++ app/Http/Resources/MessageResource.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class MessageResource extends JsonResource
 {
@@ -19,6 +21,7 @@ class MessageResource extends JsonResource
             'sender_type' => $this->sender_type,
             'message_type' => $this->message_type,
             'body' => $this->body,
+            'attachment_url' => $this->attachment ? Storage::disk('local')->temporaryUrl($this->attachment, Carbon::now()->addMinutes(5)) : null,
             'sent_at' => $this->sent_at,
         ];
     }
