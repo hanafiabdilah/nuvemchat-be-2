e18e81d81f01882754f6bb232084dca3bf8c8b86
Hanafi Abdilah <handilah81@gmail.com>
2025-11-27 13:40:57 +0700

    get conversation messages

--- app/Http/Controllers/Api/ConversationController.php
+++ app/Http/Controllers/Api/ConversationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ConversationResource;
+use App\Http\Resources\MessageResource;
 use App\Models\Conversation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -30,5 +31,19 @@ class ConversationController extends Controller
         return response()->json([
             'data' => $conversation->toResource(ConversationResource::class),
         ]);
+    }
+
+    public function messages(Request $request, int $id)
+    {
+        $page = $request->query('page', 1);
+        $per_page = min($request->query('per_page', 100), 1000);
+
+        $conversation = Conversation::whereHas('connection', function($q){
+            $q->where('user_id', Auth::id());
+        })->findOrFail($id);
+
+        $messages = $conversation->messages()->orderBy('created_at', 'DESC')->paginate($per_page, ['*'], 'page', $page);
+
+        return MessageResource::collection($messages)->response();
     }
 }
