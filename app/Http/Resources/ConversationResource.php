3cf0cef8caae8f8da576baa1f6d40d1afdb38c17
Hanafi Abdilah <handilah81@gmail.com>
2026-03-14 08:43:23 +0700

    ok

--- app/Http/Resources/ConversationResource.php
+++ app/Http/Resources/ConversationResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Enums\Message\MessageType;
+use App\Enums\Message\SenderType;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -19,7 +21,7 @@ class ConversationResource extends JsonResource
             'connection_id' => $this->connection_id,
             'last_message' => $this->last_message->toResource(MessageResource::class),
             'last_message_at' => $this->last_message_at,
-            'unread' => $this->messages()->whereNull('read_at')->count(),
+            'unread' => $this->messages()->where('sender_type', SenderType::Incoming)->whereNull('read_at')->count(),
             'contact' => new ContactResource($this->contact),
             'created_at' => $this->created_at,
         ];
