7d275dd2cd1dec2f92f3d7dfcc74a8c7c144ee3e
Hanafi Abdilah <handilah81@gmail.com>
2026-03-16 10:32:59 +0700

    test connect w api

--- app/Services/Connection/Channels/WhatsappWApiChannel.php
+++ app/Services/Connection/Channels/WhatsappWApiChannel.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Connection\Channels;
 
+use App\Enums\Connection\Status;
 use App\Models\Connection;
 use App\Services\Connection\ChannelInterface;
 
@@ -17,7 +18,32 @@ class WhatsappWApiChannel implements ChannelInterface
 
     public function connect(Connection $connection, array $data)
     {
-        //
+        validator($data, [
+            'host' => ['required', 'string'],
+            'instance_id' => ['required', 'string'],
+            'token' => ['required', 'string'],
+        ])->validate();
+
+        $connection->update([
+            'status' => Status::Pending,
+            'credentials' => [
+                'host' => $data['host'],
+                'instance_id' => $data['instance_id'],
+                'token' => $data['token'],
+            ],
+        ]);
+
+        // check status
+
+
+        // setup webhhook
+        // if status oke, update connection to active and save credentials, and return
+
+
+        // generate qr
+
+
+        // update connection
     }
 
     public function disconnect()
