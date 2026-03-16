81aa9f01afd162523d159adb2363fdbe644ed713
Hanafi Abdilah <handilah81@gmail.com>
2026-03-16 16:38:14 +0700

    send message w api from panel

---
+++ app/Services/Message/Handlers/WhatsappWApiHandler.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Services\Message\Handlers;
+
+use App\Enums\Message\MessageType;
+use App\Enums\Message\SenderType;
+use App\Models\Conversation;
+use App\Models\Message;
+use App\Services\Message\MessageHandlerInterface;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Support\Facades\Http;
+
+class WhatsappWApiHandler implements MessageHandlerInterface
+{
+    public function getConversationId(array $payload): string
+    {
+        return '';
+    }
+
+    public function getMessageId(array $payload): string
+    {
+        return $payload['messages']['messageId'];
+    }
+
+    public function getMessageSentAt(array $payload): Carbon
+    {
+        return Carbon::now(); // Assuming message is sent now
+    }
+
+
+    public function handleSendMessage(Conversation $conversation, array $data): ?Message
+    {
+        validator($data, [
+            'message' => 'required|string',
+        ])->validate();
+
+        $connection = $conversation->connection;
+
+        try {
+            $response = Http::withHeaders([
+                'Authorization' => 'Bearer ' . $connection->credentials['token'],
+            ])->post('https://api.w-api.app/v1/message/send-text?instanceId=' . $connection->credentials['instance_id'], [
+                'phone' => $conversation->external_id,
+                'message' => $data['message'],
+            ]);
+
+            $responseArray = $response->json();
+
+            $conversation = Conversation::firstOrCreate([
+                'connection_id' => $connection->id,
+                'external_id'   => $this->getConversationId($responseArray),
+            ]);
+
+            $message = $conversation->messages()->create([
+                'external_id' => $this->getMessageId($responseArray),
+                'sender_type' => SenderType::Outgoing,
+                'message_type' => MessageType::Text,
+                'body' => $data['message'],
+                'sent_at' => $this->getMessageSentAt($responseArray),
+                'read_at' => $this->getMessageSentAt($responseArray),
+                'meta' => $responseArray,
+            ]);
+
+            return $message;
+        } catch (\Throwable $th) {
+            throw new Exception('Failed to send WhatsApp message');
+        }
+    }
+}
--- app/Services/Message/MessageFactory.php
+++ app/Services/Message/MessageFactory.php
@@ -5,6 +5,7 @@ namespace App\Services\Message;
 use App\Enums\Connection\Channel;
 use App\Services\Message\Handlers\WhatsappOfficialHandler;
 use App\Services\Message\Handlers\TelegramHandler;
+use App\Services\Message\Handlers\WhatsappWApiHandler;
 use InvalidArgumentException;
 
 class MessageFactory
@@ -14,6 +15,7 @@ class MessageFactory
         return match($channel){
             Channel::WhatsappOfficial => new WhatsappOfficialHandler(),
             Channel::Telegram => new TelegramHandler(),
+            Channel::WhatsappWApi => new WhatsappWApiHandler(),
             default => throw new InvalidArgumentException("Unsupported channel: " . $channel->value),
         };
     }
