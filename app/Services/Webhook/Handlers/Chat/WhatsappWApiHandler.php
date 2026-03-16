1d3e57afca7b15546b14387defec11c5bc1694f6
Hanafi Abdilah <handilah81@gmail.com>
2026-03-16 13:44:20 +0700

    ok

--- app/Services/Webhook/Handlers/Chat/WhatsappWApiHandler.php
+++ app/Services/Webhook/Handlers/Chat/WhatsappWApiHandler.php
@@ -80,7 +80,7 @@ class WhatsappWApiHandler implements ChatHandlerInterface
             'credentials' => $credentials,
         ]);
 
-        broadcast(new ConnectionUpdated($connection));
+        broadcast(new ConnectionUpdated($connection->fresh()));
     }
 
     private function handleDisconnected(Connection $connection, array $payload)
@@ -93,7 +93,7 @@ class WhatsappWApiHandler implements ChatHandlerInterface
             'credentials' => $credentials,
         ]);
 
-        broadcast(new ConnectionUpdated($connection));
+        broadcast(new ConnectionUpdated($connection->fresh()));
     }
 }
 
