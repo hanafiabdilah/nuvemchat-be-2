eb3148a25fc52684af6debfa3e8505c9f88f8c27
Hanafi Abdilah <handilah81@gmail.com>
2026-03-18 09:16:02 +0700

    ok

--- app/Http/Controllers/Api/ConversationController.php
+++ app/Http/Controllers/Api/ConversationController.php
@@ -18,7 +18,7 @@ class ConversationController extends Controller
     {
         $conversations = Conversation::with('contact')->whereHas('connection', function($q){
             $q->where('user_id', Auth::id());
-        })->orderBy('last_message_at', 'DESC')->orderBy('id', 'DESC')->cursorPaginate(1);
+        })->orderBy('last_message_at', 'DESC')->orderBy('id', 'DESC')->cursorPaginate(50);
 
         return ConversationResource::collection($conversations)->response();
     }
@@ -40,7 +40,7 @@ class ConversationController extends Controller
             $q->where('user_id', Auth::id());
         })->findOrFail($id);
 
-        $messages = $conversation->messages()->orderBy('created_at', 'DESC')->orderBy('id', 'DESC')->cursorPaginate(1);
+        $messages = $conversation->messages()->orderBy('created_at', 'DESC')->orderBy('id', 'DESC')->cursorPaginate(50);
 
         return MessageResource::collection($messages)->response();
     }
