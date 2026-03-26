a7e000ab7864386c0e7526de89e5f63881dbee74
Hanafi Abdilah <handilah81@gmail.com>
2026-03-26 15:06:31 +0700

    ok

--- app/Services/Webhook/Handlers/Chat/TelegramHandler.php
+++ app/Services/Webhook/Handlers/Chat/TelegramHandler.php
@@ -130,7 +130,7 @@ class TelegramHandler implements ChatHandlerInterface
             }
 
             broadcast(new MessageReceived($message));
-            broadcast(new ConversationUpdated($message->conversation->wasRecentlyCreated ? $message->conversation->load('contact') : $message->conversation));
+            broadcast(new ConversationUpdated($message->conversation->load('contact')));
         }
     }
 
