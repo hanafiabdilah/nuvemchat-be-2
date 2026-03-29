7a8605b86759b31307171433e4fbc4c898a15705
Hanafi Abdilah <handilah81@gmail.com>
2026-03-29 18:18:28 +0700

    ensure owner middlewre

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Api\ConversationController;
 use App\Http\Controllers\Api\MessageController;
 use App\Http\Controllers\Api\TagController;
 use App\Http\Controllers\Api\V1\SendMessageController;
+use App\Http\Middleware\EnsureOwner;
 use App\Http\Middleware\V1\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -22,27 +23,29 @@ Route::middleware('auth:sanctum')->group(function(){
 
     Route::get('/conversations', [ConversationController::class, 'index']);
     Route::get('/conversations/{id}', [ConversationController::class, 'show']);
-    Route::get('/conversations/{id}/messages', [ConversationController::class, 'messages']);
+    // Route::get('/conversations/{id}/messages', [ConversationController::class, 'messages']);
     Route::post('/conversations/{id}/messages', [ConversationController::class, 'sendMessage']);
     Route::get('/conversations/{id}/read', [ConversationController::class, 'read']);
     Route::post('/conversations/{id}/accept', [ConversationController::class, 'accept']);
     Route::post('/conversations/{id}/resolve', [ConversationController::class, 'resolve']);
     Route::post('/conversations/{id}/tags', [ConversationController::class, 'syncTags']);
 
-    Route::get('/connections', [ConnectionController::class, 'index']);
-    Route::post('/connections', [ConnectionController::class, 'store']);
-    Route::post('/connections/{id}/connect', [ConnectionController::class, 'connect']);
-    Route::post('/connections/{id}/generate-api-key', [ConnectionController::class, 'generateApiKey']);
+    Route::middleware(EnsureOwner::class)->group(function(){
+        Route::get('/connections', [ConnectionController::class, 'index']);
+        Route::post('/connections', [ConnectionController::class, 'store']);
+        Route::post('/connections/{id}/connect', [ConnectionController::class, 'connect']);
+        Route::post('/connections/{id}/generate-api-key', [ConnectionController::class, 'generateApiKey']);
 
-    Route::get('/tags', [TagController::class, 'index']);
-    Route::post('/tags', [TagController::class, 'store']);
-    Route::put('/tags/{id}', [TagController::class, 'update']);
-    Route::delete('/tags/{id}', [TagController::class, 'destroy']);
+        Route::get('/tags', [TagController::class, 'index']);
+        Route::post('/tags', [TagController::class, 'store']);
+        Route::put('/tags/{id}', [TagController::class, 'update']);
+        Route::delete('/tags/{id}', [TagController::class, 'destroy']);
 
-    Route::get('/agents', [AgentController::class, 'index']);
-    Route::post('/agents', [AgentController::class, 'store']);
-    Route::put('/agents/{id}', [AgentController::class, 'update']);
-    Route::delete('/agents/{id}', [AgentController::class, 'destroy']);
+        Route::get('/agents', [AgentController::class, 'index']);
+        Route::post('/agents', [AgentController::class, 'store']);
+        Route::put('/agents/{id}', [AgentController::class, 'update']);
+        Route::delete('/agents/{id}', [AgentController::class, 'destroy']);
+    });
 });
 
 Route::prefix('/v1')->middleware(Auth::class)->group(function(){
