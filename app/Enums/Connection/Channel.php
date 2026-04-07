cb00e432c494008d40ea326066e352e3e7733063
Hanafi Abdilah <handilah81@gmail.com>
2026-04-07 10:47:46 +0700

    add instagram

--- app/Enums/Connection/Channel.php
+++ app/Enums/Connection/Channel.php
@@ -4,6 +4,7 @@ namespace App\Enums\Connection;
 
 enum Channel: string
 {
+    case Instagram = 'instagram';
     case WhatsappOfficial = 'whatsapp_official';
     case WhatsappWApi = 'whatsapp_w_api';
     case Telegram = 'telegram';
