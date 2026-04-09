43a63c8476d1f2d3c5a6ffb1bd81c472b1f0e7bc
Hanafi Abdilah <handilah81@gmail.com>
2026-04-09 19:25:35 +0700

    ok

--- app/Http/Controllers/Webhook/InstagramController.php
+++ app/Http/Controllers/Webhook/InstagramController.php
@@ -32,7 +32,7 @@ class InstagramController extends Controller
 
     public function handle(Request $request)
     {
-        Log::info('Instagram webhook received', json_encode($request->all()));
+        Log::info('Instagram webhook received', ['payload' => json_encode($request->all())]);
 
         $object = $request->input('object');
 
