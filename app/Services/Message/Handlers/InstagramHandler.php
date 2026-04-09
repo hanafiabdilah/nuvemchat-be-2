3dd82bd8e21b1b6572e284ce0db854c8caa03255
Hanafi Abdilah <handilah81@gmail.com>
2026-04-09 17:06:09 +0700

    send media meessage

--- app/Services/Message/Handlers/InstagramHandler.php
+++ app/Services/Message/Handlers/InstagramHandler.php
@@ -82,21 +82,249 @@ class InstagramHandler implements MessageHandlerInterface
 
     public function handleSendImage(Conversation $conversation, array $data): ?Message
     {
-        throw new Exception('Image sending not implemented for Instagram API');
+        validator($data, [
+            'url' => 'required|string|url',
+        ])->validate();
+
+        $connection = $conversation->connection;
+
+        try {
+            $response = Http::withToken($connection->credentials['access_token'])
+                ->post('https://graph.instagram.com/v25.0/me/messages', [
+                    'recipient' => [
+                        'id' => $conversation->external_id,
+                    ],
+                    'message' => [
+                        'attachment' => [
+                            'type' => 'image',
+                            'payload' => [
+                                'url' => $data['url'],
+                                'is_reusable' => $data['is_reusable'] ?? false,
+                            ],
+                        ],
+                    ],
+                ]);
+
+            $responseArray = $response->json();
+
+            if (!$response->successful()) {
+                Log::error('InstagramHandler: Failed to send image', [
+                    'response' => $responseArray,
+                    'conversation_id' => $conversation->id,
+                ]);
+                throw new Exception($responseArray['error']['message'] ?? 'Failed to send Instagram image');
+            }
+
+            Log::info('InstagramHandler: Image sent successfully', [
+                'response' => $responseArray,
+                'conversation_id' => $conversation->id,
+            ]);
+
+            $message = $conversation->messages()->create([
+                'external_id' => $this->getMessageId($responseArray),
+                'sender_type' => SenderType::Outgoing,
+                'message_type' => MessageType::Image,
+                'body' => $data['url'],
+                'sent_at' => $this->getMessageSentAt($responseArray),
+                'delivery_at' => $this->getMessageSentAt($responseArray),
+                'meta' => $responseArray,
+            ]);
+
+            return $message;
+        } catch (\Throwable $th) {
+            Log::error('InstagramHandler: Failed to send image', [
+                'error' => $th->getMessage(),
+                'conversation_id' => $conversation->id,
+                'connection_id' => $connection->id,
+            ]);
+
+            throw new Exception('Failed to send Instagram image: ' . $th->getMessage());
+        }
     }
 
     public function handleSendAudio(Conversation $conversation, array $data): ?Message
     {
-        throw new Exception('Audio sending not implemented for Instagram API');
+        validator($data, [
+            'url' => 'required|string|url',
+        ])->validate();
+
+        $connection = $conversation->connection;
+
+        try {
+            $response = Http::withToken($connection->credentials['access_token'])
+                ->post('https://graph.instagram.com/v25.0/me/messages', [
+                    'recipient' => [
+                        'id' => $conversation->external_id,
+                    ],
+                    'message' => [
+                        'attachment' => [
+                            'type' => 'audio',
+                            'payload' => [
+                                'url' => $data['url'],
+                                'is_reusable' => $data['is_reusable'] ?? false,
+                            ],
+                        ],
+                    ],
+                ]);
+
+            $responseArray = $response->json();
+
+            if (!$response->successful()) {
+                Log::error('InstagramHandler: Failed to send audio', [
+                    'response' => $responseArray,
+                    'conversation_id' => $conversation->id,
+                ]);
+                throw new Exception($responseArray['error']['message'] ?? 'Failed to send Instagram audio');
+            }
+
+            Log::info('InstagramHandler: Audio sent successfully', [
+                'response' => $responseArray,
+                'conversation_id' => $conversation->id,
+            ]);
+
+            $message = $conversation->messages()->create([
+                'external_id' => $this->getMessageId($responseArray),
+                'sender_type' => SenderType::Outgoing,
+                'message_type' => MessageType::Audio,
+                'body' => $data['url'],
+                'sent_at' => $this->getMessageSentAt($responseArray),
+                'delivery_at' => $this->getMessageSentAt($responseArray),
+                'meta' => $responseArray,
+            ]);
+
+            return $message;
+        } catch (\Throwable $th) {
+            Log::error('InstagramHandler: Failed to send audio', [
+                'error' => $th->getMessage(),
+                'conversation_id' => $conversation->id,
+                'connection_id' => $connection->id,
+            ]);
+
+            throw new Exception('Failed to send Instagram audio: ' . $th->getMessage());
+        }
     }
 
     public function handleSendVideo(Conversation $conversation, array $data): ?Message
     {
-        throw new Exception('Video sending not implemented for Instagram API');
+        validator($data, [
+            'url' => 'required|string|url',
+        ])->validate();
+
+        $connection = $conversation->connection;
+
+        try {
+            $response = Http::withToken($connection->credentials['access_token'])
+                ->post('https://graph.instagram.com/v25.0/me/messages', [
+                    'recipient' => [
+                        'id' => $conversation->external_id,
+                    ],
+                    'message' => [
+                        'attachment' => [
+                            'type' => 'video',
+                            'payload' => [
+                                'url' => $data['url'],
+                                'is_reusable' => $data['is_reusable'] ?? false,
+                            ],
+                        ],
+                    ],
+                ]);
+
+            $responseArray = $response->json();
+
+            if (!$response->successful()) {
+                Log::error('InstagramHandler: Failed to send video', [
+                    'response' => $responseArray,
+                    'conversation_id' => $conversation->id,
+                ]);
+                throw new Exception($responseArray['error']['message'] ?? 'Failed to send Instagram video');
+            }
+
+            Log::info('InstagramHandler: Video sent successfully', [
+                'response' => $responseArray,
+                'conversation_id' => $conversation->id,
+            ]);
+
+            $message = $conversation->messages()->create([
+                'external_id' => $this->getMessageId($responseArray),
+                'sender_type' => SenderType::Outgoing,
+                'message_type' => MessageType::Video,
+                'body' => $data['url'],
+                'sent_at' => $this->getMessageSentAt($responseArray),
+                'delivery_at' => $this->getMessageSentAt($responseArray),
+                'meta' => $responseArray,
+            ]);
+
+            return $message;
+        } catch (\Throwable $th) {
+            Log::error('InstagramHandler: Failed to send video', [
+                'error' => $th->getMessage(),
+                'conversation_id' => $conversation->id,
+                'connection_id' => $connection->id,
+            ]);
+
+            throw new Exception('Failed to send Instagram video: ' . $th->getMessage());
+        }
     }
 
     public function handleSendDocument(Conversation $conversation, array $data): ?Message
     {
-        throw new Exception('Document sending not implemented for Instagram API');
+        validator($data, [
+            'url' => 'required|string|url',
+        ])->validate();
+
+        $connection = $conversation->connection;
+
+        try {
+            $response = Http::withToken($connection->credentials['access_token'])
+                ->post('https://graph.instagram.com/v25.0/me/messages', [
+                    'recipient' => [
+                        'id' => $conversation->external_id,
+                    ],
+                    'message' => [
+                        'attachment' => [
+                            'type' => 'file',
+                            'payload' => [
+                                'url' => $data['url'],
+                                'is_reusable' => $data['is_reusable'] ?? false,
+                            ],
+                        ],
+                    ],
+                ]);
+
+            $responseArray = $response->json();
+
+            if (!$response->successful()) {
+                Log::error('InstagramHandler: Failed to send document', [
+                    'response' => $responseArray,
+                    'conversation_id' => $conversation->id,
+                ]);
+                throw new Exception($responseArray['error']['message'] ?? 'Failed to send Instagram document');
+            }
+
+            Log::info('InstagramHandler: Document sent successfully', [
+                'response' => $responseArray,
+                'conversation_id' => $conversation->id,
+            ]);
+
+            $message = $conversation->messages()->create([
+                'external_id' => $this->getMessageId($responseArray),
+                'sender_type' => SenderType::Outgoing,
+                'message_type' => MessageType::Document,
+                'body' => $data['url'],
+                'sent_at' => $this->getMessageSentAt($responseArray),
+                'delivery_at' => $this->getMessageSentAt($responseArray),
+                'meta' => $responseArray,
+            ]);
+
+            return $message;
+        } catch (\Throwable $th) {
+            Log::error('InstagramHandler: Failed to send document', [
+                'error' => $th->getMessage(),
+                'conversation_id' => $conversation->id,
+                'connection_id' => $connection->id,
+            ]);
+
+            throw new Exception('Failed to send Instagram document: ' . $th->getMessage());
+        }
     }
 }
